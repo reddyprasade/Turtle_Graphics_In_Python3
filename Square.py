bd1e09a16577b0e24a48d23b9282bb64b7d63920
REDDY PRASAD <15084222+reddyprasade@users.noreply.github.com>
2020-04-11 07:46:47 +0530

    Add files via upload

---
+++ Square.py
@@ -0,0 +1,13 @@
+# Python program to draw square 
+# using Turtle Programming 
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("light green")
+skk = turtle.Turtle()
+
+
+for i in range(5): 
+	skk.forward(50) 
+	skk.right(90) 
+	
+turtle.done() 
---
+++ Star.py
@@ -0,0 +1,11 @@
+# Python program to draw star 
+# using Turtle Programming 
+import turtle 
+
+star = turtle.Turtle() 
+
+for i in range(50): 
+	star.forward(50) 
+	star.right(144) 
+	
+turtle.done() 
